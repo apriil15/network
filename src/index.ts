775ca5c9d98a50fb7dce188ea3f4330423e9f368
nlwkobe30 <nlwkobe30@gmail.com>
2023-03-21 22:56:28 +0800

    docs: update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+// 1. "npm run start"
+// 2. open other terminal and run "nc -u 127.0.0.1 5500"
+
+// -u: UDP mode
+
 import { createSocket } from 'dgram'
 
 const socket = createSocket('udp4')
@@ -7,6 +12,3 @@ socket.bind(5500, '127.0.0.1')
 socket.on('message', (msg, info) => {
   console.log(msg, info)
 })
-
-// use command to test: "nc -u 127.0.0.1 5500"
-// -u: UDP mode
